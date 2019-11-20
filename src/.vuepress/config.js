683e2e70e3be2dc25b967ef9773af27b0e09abf9
nokazn <baleariclemon@gmail.com>
2019-11-20 01:00:50 +0000

    tips/javascript/undefined page

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -2,5 +2,28 @@ module.exports = {
   title: 'nokazn\'s Notes',
   description: 'nokazn のメモ',
   dest: 'docs/',
-  base: '/nokazn-notes/'
+  base: '/nokazn-notes/',
+  lang: 'ja-JP',
+  markdown: {
+    lineNumbers: true
+  },
+  plugins: [
+    ['@vuepress/back-to-top', true],
+    ['@vuepress/last-updated']
+  ],
+  themeConfig: {
+    lastUpdated: 'Last Updated',
+    nav: [
+      {
+        text: 'GitHub',
+        link: 'https://github.com/nokazn',
+        target: '_blank'
+      }
+    ],
+    sidebar: [
+      '/',
+      '/tips/javascript/undefined'
+    ],
+    smoothScroll: true
+  }
 }
---
+++ src/index.md
@@ -0,0 +1,5 @@
+# tips
+
+## JavaSctript
+
+- [undefined](/tips/javascript/undefined.md)
